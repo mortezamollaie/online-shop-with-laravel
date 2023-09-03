037a116fd5a0f822194b6a89cae02b535f366ce5
morteza <mm.gov.1381@gmail.com>
2023-09-03 13:56:47 +0330

    add route for admin panel and make admin dashboard controller

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// admin url
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->namespace('Admin')->group(function(){
+    Route::get('/', [AdminDashboardController::Class, 'index'])->name('admin.home');
 });
+
+
