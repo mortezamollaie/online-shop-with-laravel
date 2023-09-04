2f5655ebf0cee8fa9a6cab5c3510f399b2c0eb39
morteza <mm.gov.1381@gmail.com>
2023-09-04 16:18:56 +0330

    finish category create page

--- resources/views/admin/market/category/index.blade.php
+++ resources/views/admin/market/category/index.blade.php
@@ -20,7 +20,7 @@
                     <h4>دسته بندی</h4>
                 </section>
                 <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                    <a href="#" class="btn btn-info btn-sm">ایجاد دسته بندی</a>
+                    <a href="{{ route('admin.market.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته بندی</a>
                     <div class="max-width-16-rem">
                         <input type="text" class="form-control form-control-sm form-text" placeholder="جستجو">
                     </div>
