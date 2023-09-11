925fa82d864eafa3606a151096dc37ceaeea9a26
morteza <mm.gov.1381@gmail.com>
2023-09-11 11:30:14 +0330

    design user migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,22 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('mobile')->unique()->nullable();
+            $table->string('national-code')->unique()->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('slug')->unique()->nullable();
+            $table->text('profile_photo_path')->nullable()->comment('avatar');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('activation')->default(0)->comment('0 => inactive, 1 => acctive');
+            $table->timestamp('activation_date')->nullable();
+            $table->tinyInteger('user_type')->default(0)->comment('0 => user, 1 => admin');
+            $table->tinyInteger('status')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
