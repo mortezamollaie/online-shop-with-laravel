8b7ce05ffda7ef842e10b8748e27d77a64c5b656
morteza <mm.gov.1381@gmail.com>
2023-09-16 19:05:30 +0330

    convert errors to persian

--- app/Http/Requests/Admin/Content/PostCategoryRequest.php
+++ app/Http/Requests/Admin/Content/PostCategoryRequest.php
@@ -41,14 +41,4 @@ class PostCategoryRequest extends FormRequest
             ];
         }
     }
-
-    public function attributes(){
-        return[
-            'name' => 'نام دسته بندی',
-            'description' => 'توضیحات',
-            'image' => 'تصویر',
-            'status' => 'وضعیت',
-            'tags' => 'تگ ها'
-        ];
-    }
 }
