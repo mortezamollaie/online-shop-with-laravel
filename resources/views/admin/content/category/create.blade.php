ef7ef7b3fec0f1e347c1022ad72f7e4974362982
morteza <mm.gov.1381@gmail.com>
2023-09-16 18:53:46 +0330

    using select2 - part3

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -1,7 +1,7 @@
 @extends('admin.layouts.master')
 
 @section('head-tag')
-    <title>ایجاد دسته بندی</title>
+    <title>دسته بندی</title>
 @endsection
 
 @section('content')
@@ -10,25 +10,30 @@
             <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">دسته بندی</a></li>
-            <li class="breadcrumb-item active font-size-12" aria-current="page"> ایجاد دسته بندی</li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> ایجاد دسته بندی</li>
         </ol>
     </nav>
+
 
     <section class="row">
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h5>ایجاد دسته بندی</h5>
+                    <h5>
+                        ایجاد دسته بندی
+                    </h5>
                 </section>
+
                 <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
                     <a href="{{ route('admin.content.category.index') }}" class="btn btn-info btn-sm">بازگشت</a>
                 </section>
 
-                <section class="">
-                    <form action="{{ route('admin.content.category.store') }}" method="POST" enctype="multipart/form-data"
+                <section>
+                    <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data"
                         id="form">
                         @csrf
                         <section class="row">
+
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="name">نام دسته</label>
@@ -43,13 +48,14 @@
                                     </span>
                                 @enderror
                             </section>
+
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
                                     <input type="hidden" class="form-control form-control-sm" name="tags" id="tags"
                                         value="{{ old('tags') }}">
-                                    <select name="" id="select_tags" class="select2 form-control form-control-sm"
-                                        multiple>
+                                    <select class="select2 form-control form-control-sm" id="select_tags" multiple>
+
                                     </select>
                                 </div>
                                 @error('tags')
@@ -64,10 +70,11 @@
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
-                                    <select name="status" id="status" class="form-control form-control-sm">
-                                        <option value="0" @if (old('status') === 0) selected @endif>غیرفعال
+                                    <select name="status" id="" class="form-control form-control-sm"
+                                        id="status">
+                                        <option value="0" @if (old('status') == 0) selected @endif>غیرفعال
                                         </option>
-                                        <option value="1" @if (old('status') === 1) selected @endif>فعال
+                                        <option value="1" @if (old('status') == 1) selected @endif>فعال
                                         </option>
                                     </select>
                                 </div>
@@ -83,8 +90,8 @@
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="image">تصویر</label>
-                                    <input type="file" class="form-control form-control-sm" name="image" id="image"
-                                        value="{{ old('image') }}">
+                                    <input type="file" class="form-control form-control-sm" name="image"
+                                        id="image">
                                 </div>
                                 @error('image')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -95,9 +102,13 @@
                                 @enderror
                             </section>
 
-                            <section class="col-12 my-2">
-                                <div class="form-group"><label for="description">توضیحات</label>
-                                    <textarea name="description" id="description" rows="4">{{ old('description') }}</textarea>
+
+                            <section class="col-12">
+                                <div class="form-group">
+                                    <label for="">توضیحات</label>
+                                    <textarea name="description" id="description" class="form-control form-control-sm" rows="6">
+                                    {{ old('description') }}
+                                </textarea>
                                 </div>
                                 @error('description')
                                     <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
@@ -108,12 +119,14 @@
                                 @enderror
                             </section>
 
-                            <section class="col-12 my-2">
+
+                            <section class="col-12 my-3">
                                 <button class="btn btn-primary btn-sm">ثبت</button>
                             </section>
                         </section>
                     </form>
                 </section>
+
             </section>
         </section>
     </section>
@@ -129,14 +142,25 @@
         $(document).ready(function() {
             var tags_input = $('#tags');
             var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
+
+            if (tags_input.val() !== null && tags_input.val().length > 0) {
+                default_data = default_tags.split(',');
+            }
+
             select_tags.select2({
                 placeholder: 'لطفا تگ های خود را وارد نمایید',
-                tags: true
+                tags: true,
+                data: default_data
             });
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+
             $('#form').submit(function(event) {
                 if (select_tags.val() !== null && select_tags.val().length > 0) {
                     var selectedSource = select_tags.val().join(',');
-                    tags_input.val(selectedSource);
+                    tags_input.val(selectedSource)
                 }
             })
         })
