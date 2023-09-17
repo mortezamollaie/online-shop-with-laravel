3638444342d8091c9ff7a3513ac41d700ccc08b8
morteza <mm.gov.1381@gmail.com>
2023-09-17 15:43:03 +0330

    adding sweet alert to project - part1

--- app/Http/Controllers/Admin/Content/CategoryController.php
+++ app/Http/Controllers/Admin/Content/CategoryController.php
@@ -36,7 +36,7 @@ class CategoryController extends Controller
         $inputs['slug'] = str_replace(' ', '-', $inputs['name']) . '-' . Str::random(5);
         $inputs['image'] = 'image';
         $postCategory = PostCategory::create($inputs);
-        return redirect()->route('admin.content.category.index');
+        return redirect()->route('admin.content.category.index')->with('swal-success', 'دسته بندی جدید شما با موفقیت ثبت شد')->with('toast-success', 'دسته بندی جدید با موفقیت ثبت شد')->with('alert-section-success', 'دسته بندی جدید با موفقیت ثبت شد');
     }
 
     /**
