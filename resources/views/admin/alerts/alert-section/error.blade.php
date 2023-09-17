ed8df349278f4e0318f4b0a46006ed55cab52986
morteza <mm.gov.1381@gmail.com>
2023-09-17 16:09:16 +0330

    adding alert - part2

---
+++ resources/views/admin/alerts/alert-section/error.blade.php
@@ -0,0 +1,13 @@
+@if (session('alert-section-error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <h4 class="alert-heading">&times; خطا</h4>
+        <hr>
+        <p class="mb-0">
+            {{ session('alert-section-error') }}
+        </p>
+        <button type="button" class="close" date-dismiss="alert" aria-lable="Close"
+            style="right: auto!important; left: 0!important">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
