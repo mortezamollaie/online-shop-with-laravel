a9f6b2f8eb13e5c725757b33d35ccc28380e6216
morteza <mm.gov.1381@gmail.com>
2023-09-18 12:05:24 +0330

    adding alert - part4

--- resources/views/admin/content/category/index.blade.php
+++ resources/views/admin/content/category/index.blade.php
@@ -8,22 +8,25 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
-            <li class="breadcrumb-item font-size-12"> <a href="#">بخش محتوی</a></li>
-            <li class="breadcrumb-item active font-size-12" aria-current="page"> دسته بندی</li>
+            <li class="breadcrumb-item font-size-12"> <a href="#">بخش فروش</a></li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> دسته بندی</li>
         </ol>
     </nav>
+
 
     <section class="row">
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h5>دسته بندی</h5>
+                    <h5>
+                        دسته بندی
+                    </h5>
                 </section>
 
                 @include('admin.alerts.alert-section.success')
 
                 <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
-                    <a href="{{ route('admin.content.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته جدید</a>
+                    <a href="{{ route('admin.content.category.create') }}" class="btn btn-info btn-sm">ایجاد دسته بندی</a>
                     <div class="max-width-16-rem">
                         <input type="text" class="form-control form-control-sm form-text" placeholder="جستجو">
                     </div>
@@ -56,7 +59,7 @@
                                     </td>
                                     <td>{{ $postCategory->tags }}</td>
                                     <td>
-                                        <label for="">
+                                        <label>
                                             <input id="{{ $postCategory->id }}"
                                                 onchange="changeStatus({{ $postCategory->id }})"
                                                 data-url="{{ route('admin.content.category.status', $postCategory->id) }}"
@@ -65,33 +68,33 @@
                                     </td>
                                     <td class="width-16-rem text-left">
                                         <a href="{{ route('admin.content.category.edit', $postCategory->id) }}"
-                                            class="btn btn-primary btn-sm"><i class="fa fa-edit"></i>
-                                            ویرایش</a>
+                                            class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
                                         <form class="d-inline"
                                             action="{{ route('admin.content.category.destroy', $postCategory->id) }}"
-                                            method="POST">
+                                            method="post">
                                             @csrf
                                             {{ method_field('delete') }}
-                                            <button type="submit" class="btn btn-danger btn-sm"><i
-                                                    class="fa fa-trash-alt"></i>
-                                                حذف</button>
+                                            <button class="btn btn-danger btn-sm" type="submit"><i
+                                                    class="fa fa-trash-alt"></i> حذف</button>
                                         </form>
                                     </td>
                                 </tr>
                             @endforeach
+
+
                         </tbody>
                     </table>
                 </section>
+
             </section>
         </section>
     </section>
 @endsection
-
 @section('script')
     <script type="text/javascript">
         function changeStatus(id) {
-            var element = $("#" + id);
-            var url = element.attr('data-url');
+            var element = $("#" + id)
+            var url = element.attr('data-url')
             var elementValue = !element.prop('checked');
 
             $.ajax({
@@ -101,14 +104,50 @@
                     if (response.status) {
                         if (response.checked) {
                             element.prop('checked', true);
+                            successToast('دسته بندی با موفقیت فعال شد')
                         } else {
                             element.prop('checked', false);
+                            successToast('دسته بندی با موفقیت غیر فعال شد')
                         }
                     } else {
-                        element.prop('checked', elementValue)
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
                     }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
                 }
-            })
+            });
+
+            function successToast(message) {
+
+                var successToastTag =
+                    '<section class="toast" data-delay="5000">\n<section class="toast-body py-3 d-flex bg-success text-white">\n<strong class="ml-auto">' +
+                    message +
+                    '</strong>\n<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n<span aria-hidden="true">&times;</span>\n</button>\n</section>\n</section>';
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+            }
+
+            function errorToast(message) {
+
+                var errorToastTag =
+                    '<section class="toast" data-delay="5000">\n<section class="toast-body py-3 d-flex bg-danger text-white">\n<strong class="ml-auto">' +
+                    message +
+                    '</strong>\n<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+
+                $('.toast-wrapper').append(errorToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+            }
         }
     </script>
 @endsection
