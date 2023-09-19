692f767d929e5e1d95be20afe7f9f56ed4bf2efc
morteza <mm.gov.1381@gmail.com>
2023-09-19 16:00:28 +0330

    adding slug creator

--- app/Models/Content/PostCategory.php
+++ app/Models/Content/PostCategory.php
@@ -3,12 +3,21 @@
 namespace App\Models\Content;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PostCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, Sluggable;
+
+    public function sluggable():array {
+        return [
+            'slug' => [
+                'source' => 'name',
+            ]
+        ];
+    }
 
     protected $fillable = [
         'name',
