58aaab70b1e7c388efa874968c23339bbf597651
morteza <mm.gov.1381@gmail.com>
2023-09-20 13:36:47 +0330

    add upload image service-finish image service

--- app/Http/Services/Image/ImageService.php
+++ app/Http/Services/Image/ImageService.php
@@ -24,4 +24,41 @@ class ImageService extends ImageToolsService{
         $result = Image::make($image->getRealPath())->fit($width, $height)->save(public_path($this->getImageAddress()), null, $this->getImageFormat());
         return $result ? $this->getImageAddress() : false;
     }
+
+    public function createIndexAndSave($image){
+        // get data from config
+        $imageSizes = Config::get('image.index-image-sizes');
+        // set image
+        $this->setImage($image);
+        // set directory
+        $this->getImageDirectory() ?? $this->setImageDirectory(date('Y') . DIRECTORY_SEPARATOR . date('m') . DIRECTORY_SEPARATOR . date('d'));
+        $this->setImageDirectory($this->getImageDirectory() . DIRECTORY_SEPARATOR . time());
+
+        //set name
+        $this->getImageName() ?? $this->setImageName(time());
+        $imageName = $this->getImageName();
+
+        $indexArray = [];
+
+        foreach($imageSizes as $sizeAlias => $imageSize){
+            // create and set this size name
+            $currentImageName = $imageName . '_' . $sizeAlias;
+
+            $this->setImageName($currentImageName);
+
+            //execute provider
+            $this->provider();
+
+            //save image
+            $result = Image::make($image->getRealPath())->fit($imageSize['width'], $imageSize['height'])->save(public_path($this->getImageAddress()), null, $this->getImageFormat());
+            if($result){
+                $indexArray[$sizeAlias] = $this->getImageAddress();
+            }else {
+                return false;
+            }
+        }
+        $image['indexArray'] = $indexArray;
+        $image['directory'] = $this->getFinalImageDirectory();
+        $image['currentImage'] = Config::get('image.default-current-index-image');
+    }
 }
