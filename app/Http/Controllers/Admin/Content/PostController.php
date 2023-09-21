16ddf7d284b42612b93750d2d01ecdf682a740cc
morteza <mm.gov.1381@gmail.com>
2023-09-21 14:35:03 +0330

    add jalali date picker

--- app/Http/Controllers/Admin/Content/PostController.php
+++ app/Http/Controllers/Admin/Content/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin\Content;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Content\Post;
+use App\Models\Content\PostCategory;
 
 class PostController extends Controller
 {
@@ -22,7 +23,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.content.post.create');
+        $postCategories = PostCategory::all();
+        return view('admin.content.post.create', compact('postCategories'));
     }
 
     /**
@@ -30,7 +32,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request);
     }
 
     /**
