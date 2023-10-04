425c6478d0a526b929e1fe0bee9bcedec84b92eb
morteza <mm.gov.1381@gmail.com>
2023-10-04 22:12:19 +0330

    admin-user index page-part2

--- resources/views/admin/user/admin-user/index.blade.php
+++ resources/views/admin/user/admin-user/index.blade.php
@@ -9,16 +9,20 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item font-size-12"> <a href="#">خانه</a></li>
             <li class="breadcrumb-item font-size-12"> <a href="#">بخش کاربران</a></li>
-            <li class="breadcrumb-item active font-size-12" aria-current="page"> کاربران ادمین</li>
+            <li class="breadcrumb-item font-size-12 active" aria-current="page"> کاربران ادمین</li>
         </ol>
     </nav>
+
 
     <section class="row">
         <section class="col-12">
             <section class="main-body-container">
                 <section class="main-body-container-header">
-                    <h5>کاربران ادمین</h5>
+                    <h5>
+                        کاربران ادمین
+                    </h5>
                 </section>
+
                 <section class="d-flex justify-content-between align-items-center mt-4 mb-3 border-bottom pb-2">
                     <a href="{{ route('admin.user.admin-user.create') }}" class="btn btn-info btn-sm">ایجاد ادمین جدید</a>
                     <div class="max-width-16-rem">
@@ -35,31 +39,189 @@
                                 <th>شماره موبایل</th>
                                 <th>نام</th>
                                 <th>نام خانوادگی</th>
+                                <th>فعال سازی</th>
+                                <th>وضعیت</th>
                                 <th>نقش</th>
-                                <th class="max-width-22-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
+                                <th class="max-width-16-rem text-center"><i class="fa fa-cogs"></i> تنظیمات</th>
                             </tr>
                         </thead>
                         <tbody>
+
                             @foreach ($admins as $key => $admin)
+                                <tr>
+                                    <th>{{ $key + 1 }}</th>
+                                    <td>{{ $admin->email }}</td>
+                                    <td>{{ $admin->mobile }}</td>
+                                    <td>{{ $admin->first_name }}</td>
+                                    <td>{{ $admin->last_name }}</td>
+                                    <td>
+                                        <label>
+                                            <input id="{{ $admin->id }}-active"
+                                                onchange="changeActive({{ $admin->id }})"
+                                                data-url="{{ route('admin.user.admin-user.activation', $admin->id) }}"
+                                                type="checkbox" @if ($admin->activation === 1) checked @endif>
+                                        </label>
+                                    </td>
+                                    <td>
+                                        <label>
+                                            <input id="{{ $admin->id }}" onchange="changeStatus({{ $admin->id }})"
+                                                data-url="{{ route('admin.user.admin-user.status', $admin->id) }}"
+                                                type="checkbox" @if ($admin->status === 1) checked @endif>
+                                        </label>
+                                    </td>
+                                    <td>سوپر ادمین </td>
+                                    <td class="width-22-rem text-left">
+                                        <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
+                                        <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i>
+                                            ویرایش</a>
+                                        <button class="btn btn-danger btn-sm" type="submit"><i class="fa fa-trash-alt"></i>
+                                            حذف</button>
+                                    </td>
+                                </tr>
                             @endforeach
-                            <tr>
-                                <th>{{ $key + 1 }}</th>
-                                <td>{{ $admin->id }}</td>
-                                <td>{{ $admin->mobile }}</td>
-                                <td>{{ $admin->first_name }}</td>
-                                <td>{{ $admin->last_name }}</td>
-                                <td>ادمین</td>
-                                <td class="width-22-rem text-left">
-                                    <a href="#" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i> نقش</a>
-                                    <a href="#" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i> ویرایش</a>
-                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash-alt"></i>
-                                        حذف</button>
-                                </td>
-                            </tr>
+
+
                         </tbody>
                     </table>
                 </section>
+
             </section>
         </section>
     </section>
+@endsection
+
+@section('script')
+    <script type="text/javascript">
+        function changeStatus(id) {
+            var element = $("#" + id)
+            var url = element.attr('data-url')
+            var elementValue = !element.prop('checked');
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response) {
+                    if (response.status) {
+                        if (response.checked) {
+                            element.prop('checked', true);
+                            successToast('ادمین  با موفقیت فعال شد')
+                        } else {
+                            element.prop('checked', false);
+                            successToast('ادمین با موفقیت غیر فعال شد')
+                        }
+                    } else {
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
+                    }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
+                }
+            });
+
+            function successToast(message) {
+
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+            }
+
+            function errorToast(message) {
+
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-danger text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+
+                $('.toast-wrapper').append(errorToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+            }
+        }
+    </script>
+
+
+    <script type="text/javascript">
+        function changeActive(id) {
+            var element = $("#" + id + '-active')
+            var url = element.attr('data-url')
+            var elementValue = !element.prop('checked');
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                success: function(response) {
+                    if (response.status) {
+                        if (response.checked) {
+                            element.prop('checked', true);
+                            successToast('فعال سازی ادمین با موفقیت انجام شد')
+                        } else {
+                            element.prop('checked', false);
+                            successToast('غیر فعال سازی ادمین با موفقیت انجام شد')
+                        }
+                    } else {
+                        element.prop('checked', elementValue);
+                        errorToast('هنگام ویرایش مشکلی بوجود امده است')
+                    }
+                },
+                error: function() {
+                    element.prop('checked', elementValue);
+                    errorToast('ارتباط برقرار نشد')
+                }
+            });
+
+            function successToast(message) {
+
+                var successToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-success text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+
+                $('.toast-wrapper').append(successToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+            }
+
+            function errorToast(message) {
+
+                var errorToastTag = '<section class="toast" data-delay="5000">\n' +
+                    '<section class="toast-body py-3 d-flex bg-danger text-white">\n' +
+                    '<strong class="ml-auto">' + message + '</strong>\n' +
+                    '<button type="button" class="mr-2 close" data-dismiss="toast" aria-label="Close">\n' +
+                    '<span aria-hidden="true">&times;</span>\n' +
+                    '</button>\n' +
+                    '</section>\n' +
+                    '</section>';
+
+                $('.toast-wrapper').append(errorToastTag);
+                $('.toast').toast('show').delay(5500).queue(function() {
+                    $(this).remove();
+                })
+            }
+        }
+    </script>
+
+
+    @include('admin.alerts.sweetalert.delete-confirm', ['className' => 'delete'])
 @endsection
